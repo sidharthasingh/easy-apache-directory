ee98345e080ad68aa98b3f77c85a250ea12e3a2d
Sidhartha Singh <15666676+sidharthasingh@users.noreply.github.com>
2017-10-02 13:42:46 +0530

    Search changes made

--- main.js
+++ main.js
@@ -10,22 +10,25 @@ function home(){
 	window.location = "/";
 }
 
+// Go to home by clicking on header
 $(".header").click(home);
+
+// GET parameters
 paraString = window.location.search; 
 
 if(paraString=="")
 	loadForm();
+else
+{
+	if(paraString[0]=="?")
+		paraString=paraString.substr(1);
+}
 
 function formSubmit(){
 	link = $("#hyperlink")[0].value;
 	if(link.indexOf("http") == -1)
 		link = "http://"+link;
 	console.log(link);
-	$.ajax({
-		url:link,
-		crossDomain:true
-	}).done(function(data){
-		console.log(data);
-	});
+	window.location = window.location.origin+"/?"+link;
 	return false;
 }
