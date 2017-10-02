4e4fb1581f388ff04edc824c5bca00b119c290cd
Sidhartha Singh <15666676+sidharthasingh@users.noreply.github.com>
2017-10-02 15:33:28 +0530

    Voila. 4 hours of hack completed. CSS to be added

--- main.js
+++ main.js
@@ -22,6 +22,19 @@ else
 {
 	if(paraString[0]=="?")
 		paraString=paraString.substr(1);
+
+	j=-1;
+	for(i=paraString.indexOf("://")+3;i<paraString.length;i++)
+		if(paraString[i] == "/")
+		{
+			j=i;
+			break;
+		}
+	if(j!=-1)
+		url = paraString.substr(0,j);
+	else
+		url = paraString;
+
 	$.get(paraString)
 		.done((data) => {
 			bi = data.indexOf("<body>");
@@ -41,17 +54,38 @@ else
 			}
 			// Now we have got an array of rows where row[i] has 4 'td' (columns)
 			dat = [];
-			for(i=0;i<rows.length;i++)
+			for(i=0;i<rows.length-1;i++)
 			{
 				obj = {};
-				temp = $.parseHTML(rows[0][0].innerHTML);
+				temp = $.parseHTML(rows[i][0].innerHTML);
 				obj.src = temp[0].src;
-				obj.href = $.parseHTML(temp[1].innerHTML)[0].href;
-				obj.src = obj.src.replace(window.location.origin,"");
-				obj.href = obj.href.replace(window.location.origin,"");
+				tempParse = $.parseHTML(temp[1].innerHTML)[0]; 
+				obj.href = tempParse.href;
+				obj.text = tempParse.innerText;
+				src = obj.src = obj.src.replace(window.location.origin,"");
+				href = obj.href = obj.href.replace(window.location.origin,"");
+				if(src.indexOf("folder.gif")==-1)
+				{
+					href = paraString+href;
+				}
+				else
+				{
+					href = window.location.origin+"/?"+paraString+href;
+				}
+				obj.src = url+src;
+				obj.href = href;
 				dat.push(obj);
+
 			}
 			console.log(dat);
+			table ="<table>"
+			for(i=0;i<dat.length;i++)
+			{
+				obj = dat[i];
+				table+="<tr><img src='"+obj.src+"'><a href='"+obj.href+"'>"+obj.text+"</a></tr><br>";
+			}
+			table+="</table>";
+			$("#view").html(table);
 		});
 }
 
